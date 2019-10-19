a27a6a94600c8bbaa71f6fa040c0c6ee94eef690
Emily Choi <emilyc@orionhealth.com>
2019-10-19 15:06:44 +1300

    Added add cart functionality and modified display of minicart

--- assets/js/mycart.js
+++ assets/js/mycart.js
@@ -1,17 +1,62 @@
+let itemCount = parseInt($(".minicart-item-count").text());
+let smallItemCount = parseInt($(".small-minicart-count").text());
+let mediumItemCount = parseInt($(".medium-minicart-count").text());
+let largeItemCount = parseInt($(".large-minicart-count").text());
+
 $(".minicart-btn").mouseenter(function() {
     //Calculate the right side position of minicart display when hovered over.
-    let headerPosition = $(".minicart-btn").position();
+    if (itemCount > 0) {
+        let headerPosition = $(".minicart-btn").position();
 
-    $(".minicart").css("top", headerPosition.top);
+        $(".minicart").css("top", headerPosition.top);
 
-    let right = ($(window).width() - (headerPosition.left + $(".minicart-btn").outerWidth(true)));
-    $(".minicart").css("right", right);
+        let right = ($(window).width() - (headerPosition.left + $(".minicart-btn").outerWidth(true)));
+        $(".minicart").css("right", right);
 
-    $(".minicart").show();
-    $(".minicart-btn").css("color", "black");
+        $(".minicart").show();
+        $(".minicart-btn").css("color", "black");
+    }
 });
 
 $(".minicart").mouseleave(function() {
     $(".minicart").hide();
     $(".minicart-btn").css("color", "#888888");
 })
+
+$(".add-button").click(function() {
+    if (checkIfSizeSelected($("#small-btn"))) {
+        if (smallItemCount === 0) {
+            console.log("Item" + itemCount);
+            $("#small-minicart-item").css("display", "flex");
+        }
+        smallItemCount++;
+        itemCount++;
+        $(".small-minicart-count").text(smallItemCount.toString());
+    }
+    else if (checkIfSizeSelected($("#medium-btn"))) {
+        if (mediumItemCount === 0) {
+            $("#medium-minicart-item").css("display", "flex");
+        }
+        mediumItemCount++;
+        itemCount++;
+        $(".medium-minicart-count").text(mediumItemCount.toString());
+    }
+    else if (checkIfSizeSelected($("#large-btn"))) {
+        if (largeItemCount === 0) {
+            $("#large-minicart-item").css("display", "flex");
+        }
+        largeItemCount++;
+        itemCount++;
+        $(".large-minicart-count").text(largeItemCount.toString());
+    }
+    else {
+        window.alert("Please select size");
+    }
+
+    $(".minicart-item-count").text(itemCount.toString());
+});
+
+function checkIfSizeSelected(className) {
+    return className.hasClass("selected-size-btn");
+}
+
